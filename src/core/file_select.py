8739bfeae2b23a4e40390e4627d4675618cf2abb
ESJIAN <3340580252@qq.com>
2025-05-10 16:05:14 +0800

    refactor(core): rename auto_file_detect.py to file_select.py and update TODO
    - Rename auto_file_detect.py to file_select.py
    - Update TODO.md with new task for auto package import
    - Add internal class organization and UI component initialization comments in main_window.py
    - Remove unnecessary blank lines in main_window.py

--- src/core/file_select.py
+++ src/core/file_select.py
@@ -2,7 +2,7 @@
 # @Time    : 2025/5/8 11:48
 # @Author  : ESJIAN
 # @Email   : esjian@outlook.com
-# @File    : ui_utils.py
+# @File    : auto_file_detect.py
 # @Software: VsCode
 
 def auto_file_detect(self):
