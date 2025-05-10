8707effef0e315a608ff799f1f4301866b4e07e8
ESJIAN <3340580252@qq.com>
2025-05-10 20:07:47 +0800

    feat(main_winodw_test.py): rearrange and enable backup buttons
    - Uncommented and repositioned the backup buttons in the UI layout
    - Swapped positions of 'backup_button' and 'backup_manager_button'
    - Updated button labels accordingly

--- src/tests/main_winodw_test.py
+++ src/tests/main_winodw_test.py
@@ -456,11 +456,11 @@ class Ui_Form(object):
         self.pushButton_115 = QPushButton(self.tab)
         self.pushButton_115.setObjectName("export_table_button")
         # # 创建立即备份按钮
-        # self.pushButton_12 = QPushButton(self.tab)
-        # self.pushButton_12.setObjectName("backup_button")
+        self.pushButton_12 = QPushButton(self.tab)
+        self.pushButton_12.setObjectName("backup_button")
         # # 创建备份管理按钮
-        # self.pushButton_11 = QPushButton(self.tab)
-        # self.pushButton_11.setObjectName("backup_manager_button")
+        self.pushButton_11 = QPushButton(self.tab)
+        self.pushButton_11.setObjectName("backup_manager_button")
 
 
 
@@ -484,8 +484,9 @@ class Ui_Form(object):
 
         #self.gridLayout_2.addWidget(self.pushButton_114,1,2,1,1) # 添加重导表格按钮位置
         self.gridLayout_2.addWidget(self.pushButton_115,1,3,1,1) # 添加导出表格按钮位置
-        # self.gridLayout_2.addWidget(self.pushButton_11,1,6,1,1) # 设置备份管理按钮位置
-        # self.gridLayout_2.addWidget(self.pushButton_12,1,7,1,1) # 设置立即备份按钮位置
+        self.gridLayout_2.addWidget(self.pushButton_12,1,7,1,1) # 设置立即备份按钮位置
+        self.gridLayout_2.addWidget(self.pushButton_11,1,6,1,1) # 设置备份管理按钮位置
+        
         
         
         self.gridLayout_3.addLayout(self.gridLayout, 0, 0, 1, 1)
@@ -555,8 +556,9 @@ class Ui_Form(object):
         "TAB底部按钮"
         # self.pushButton_114.setText(QCoreApplication.translate("Form", "重导表格", None))
         self.pushButton_115.setText(QCoreApplication.translate("Form", "导出表格", None))
-        # self.pushButton_11.setText(QCoreApplication.translate("Form", "立即备份", None))
-        # self.pushButton_12.setText(QCoreApplication.translate("Form", "备份管理", None))
+        self.pushButton_12.setText(QCoreApplication.translate("Form", "立即备份", None))
+        self.pushButton_11.setText(QCoreApplication.translate("Form", "备份管理", None))
+        
         
 
         "TAB名称"        
